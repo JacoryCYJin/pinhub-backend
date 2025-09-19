2bf8d84a35bd13b5cb886f6227ac2861294f932e
Jacory <jacory.jin@gmail.com>
2025-09-19 15:22:29 +0800

    初次提交

---
+++ src/main/java/io/github/jacorycyjin/pinhub/backend/PinhubBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.jacorycyjin.pinhub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinhubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jacorycyjin/pinhub/backend/PinhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jacorycyjin.pinhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
